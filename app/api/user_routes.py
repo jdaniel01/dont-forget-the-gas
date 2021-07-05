ab733f09c607ed2a71c389d067972b61c94c995e
James Daniel <professor4cob@outlook.com>
2021-07-04 21:50:01 -0700

    fixing bugs

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,36 +1,43 @@
 from flask import Blueprint, jsonify, request
-from flask_login import login_required, current_user
-from app.models import User
+from flask_login import login_required
+from sqlalchemy import desc
+from app.models import User, List
+from app.forms.list_form import ListForm
+from app.forms.edit_list_form import EditListForm
 
 
 user_routes = Blueprint('users', __name__)
 
 
-
-@list_routes.route('/<int:id>', methods=["PUT", "DELETE"])
+@user_routes.route('/')
 @login_required
-def updateAndDeleteList(id):
+def users():
+    users = User.query.all()
+    return {"users": [user.to_dict() for user in users]}
 
-    form = EditListForm()
+
+@user_routes.route('/<int:id>')
+@login_required
+def user(id):
+    user = User.query.get(id)
+    return user.to_dict()
+
+
+@user_routes.route('/<int:id>/lists', methods=["GET", "POST"])
+@login_required
+def addAndAllList(id):
+
+    form = ListForm()
 
     if form.validate_on_submit():
-        print("####/lists/id######### UPDATING LIST")
-        oldList = List.query.get(id)
-        if oldList:
-            oldList["name"] = form["name"]
-            oldList["list_type"] = form["list_type"]
-            oldList["notes"] = form["notes"]
-
-            db.session.commit()
-            print("####SUCCESS!! USER HAS BEEN UPDATED#####")
-
-        else:
-            print("###ERROR##ERROR## unable to locate list by primary key")
-            return "There was an Error"
+        newList = List()
+        form.populate_obj(newList)
+        print("######/users/id/lists########### populated new object preparing to commit.")
+        db.session.add(newList)
+        db.session.commit()  
+        print("#########users/id/lists########## SUCCESS! ADDED NEW LIST")
     else:
-        deleting = List.query.get(id)
-        print("#####DELETING LIST #####", deleting)
-        db.session.delete(deleting)
-        db.session.commit()
-    lists = List.query.filter_by(desc(owner_id=current_user.id)).all()
+        print("#####ERROR FORM DID NOT VALIDATE####")
+        return "Error: Form did not validate or no form was submitted"
+    lists = List.query.filter_by(desc(owner_id=id)).all()
     return lists
--- react-app/src/components/User/Profile.js
+++ react-app/src/components/User/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import List from "./List"
+import List from "../List"
 
 function Profile() {
 
