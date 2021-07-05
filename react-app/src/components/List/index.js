566469223ccc8592627dbe9570e9f121e93dd960
James Daniel <professor4cob@outlook.com>
2021-07-04 21:32:44 -0700

    added css files and organized component tree

---
+++ react-app/src/components/List/index.js
@@ -0,0 +1,16 @@
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+function List() {
+
+
+    return (
+        <div>
+            List form here
+        </div>
+    )
+}
+
+export default List;
