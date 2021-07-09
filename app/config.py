25e3b7ca69626bda76b41ceef1f79f94f32fd675
James Daniel <professor4cob@outlook.com>
2021-07-09 16:49:05 -0700

    changed database_url environ.get

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
   SECRET_KEY=os.environ.get('SECRET_KEY')
   SQLALCHEMY_TRACK_MODIFICATIONS=False
-  SQLALCHEMY_DATABASE_URI="postgresql://dftg_user:Bu5L1f3@localhost/dftg_database"
+  SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
   SQLALCHEMY_ECHO=True
   REACT_APP_GOOGLE_MAPS_API=os.environ.get("REACT_APP_GOOGLE_MAPS_API")
